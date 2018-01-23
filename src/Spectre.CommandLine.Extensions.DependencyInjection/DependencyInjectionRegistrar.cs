4049717d261b5f0eb9211297a31241126697d744
Alistair Chapman <alistair@agchapman.com>
2018-01-23 18:03:22 +1000

    Add package code and assets

---
+++ src/Spectre.CommandLine.Extensions.DependencyInjection/DependencyInjectionRegistrar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Spectre.CommandLine.Extensions.DependencyInjection
+{
+    public class DependencyInjectionRegistrar : ITypeRegistrar
+    {
+        private readonly IServiceCollection _services;
+
+        public DependencyInjectionRegistrar(IServiceCollection services)
+        {
+            _services = services;
+        }
+        public ITypeResolver Build()
+        {
+            return new DependencyInjectionResolver(_services);
+        }
+
+        public void Register(Type service, Type implementation)
+        {
+            _services.AddSingleton(service, implementation);
+        }
+
+        public void RegisterInstance(Type service, object implementation)
+        {
+            _services.AddSingleton(service, implementation);
+        }
+    }
+}
